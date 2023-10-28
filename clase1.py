9fa5ea8cdfd47ce68f3e99ed35e7410cb953e9de
DevMike08 <maescobarocampo@gmail.com>
2023-10-28 12:03:37 -0500

    Calcular el area de un cuadrado

--- clase1.py
+++ clase1.py
@@ -1,3 +1,7 @@
-
-
 # Calcular el area de un cuadrado
+base = int(input('Ingrse la base'))
+altura = int(input('Ingrese la altura'))
+
+resultado = base * altura
+
+print('El area de un cuadrado es:', resultado)
